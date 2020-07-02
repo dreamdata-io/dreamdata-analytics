a7c91e314e126696ffd44a1abef19f00eec2a291
Nikolaos Veneti <nikolaos.veneti@dreamdata.io>
2020-07-02 17:28:48 +0200

    add version as a webpack injected variable - new package name

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,33 @@
 const path = require('path');
+const webpack = require('webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: path.join(__dirname, '/lib'),
-    resolve: {
-        extensions: ['.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [new CompressionPlugin()],
-    output: {
-        library: 'dreamdata',
-        libraryTarget: 'umd',
-        filename: 'dreamdata.min.js'
-    }
+  mode: 'production',
+  entry: path.join(__dirname, '/lib'),
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CompressionPlugin(),
+    new webpack.DefinePlugin({
+      PACKAGE_VERSION: JSON.stringify(require('./package.json').version)
+    })
+  ],
+  output: {
+    library: 'dreamdata',
+    libraryTarget: 'umd',
+    filename: 'dreamdata.min.js'
+  }
 };
